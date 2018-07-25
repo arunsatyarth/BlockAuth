a2b277eedcf70aa918d93f6243b7c6e6e444f4f1
arunsatyarth <arun.satyarth@gmail.com>
2018-07-25 10:49:12 +0530

    ms site up

--- test1/contracts/MS_Login.sol
+++ test1/contracts/MS_Login.sol
@@ -11,17 +11,9 @@ contract MS_Login {
     }
     mapping(address=> License) public access_rights;
 
-    //Authentication is a 2 step process. 
-    //Step1: You send an address to the server and say that this is your address
-    //Step2: You have to prove it. You can do this by sending your private key to the server but you dont want to do that. 
-    //So it happens in the following way.
-    //Server maintain a map of address to uuid and says now mention this uuid in any transaction that comes from this address
-    //At client side take that uuid and from that address, create a transaction by calling login mentioning the uuid
-    //At client we  will store the uuid to address map in the contract
-    //Authentication wil only be possible if the uuid pointed address is the same one which made the change in  login_details_rev
 
-    mapping(address=> string) public login_details;//filled by server
-    mapping(string=> address)  login_details_rev;//filled by client
+    mapping(address=> string) public login_details;
+    mapping(string=> address)  login_details_rev;
     
     constructor() public {
         owner = msg.sender;
@@ -31,11 +23,9 @@ contract MS_Login {
         if (msg.sender == owner) _;
     }
 
-    //Executed by server
     function grant_access()public restricted{
 
     }
-    //Executed by client. Not by server
     function login(string _uuid) public {
         login_details_rev[_uuid] = msg.sender;
 
@@ -45,7 +35,6 @@ contract MS_Login {
     }
     
 
-    //tells if a uuid has got a successful login. if yes read the coresponding address from login_details_rev
     function is_login(string _uuid) public returns (bool) {
         address _address = login_details_rev[_uuid];
         string _uuid_old = login_details[_address];
